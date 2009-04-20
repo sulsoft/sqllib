7849a9e433a26b9a6b7ce55a567a2901595207e0
server\vailtom <server\vailtom@cbffd26a-6331-11de-882d-8b46ad2f25d3>
2009-04-20 18:53:26 +0000

    sdasda single teste

--- teste.bat
+++ teste.bat
@@ -1,6 +1,3 @@
-@echo off
-set path=c:\PostgresPlus\8.3\bin;%path%
-cd /d d:\HB\sqlrun
-doskey x=all
-dir
-cd..
+RENATO DIZ: Primeira linha deste teste
+RENATO DIZ: Ponha o seu nome seguido de 2 pontos para eu 
+RENATO DIZ: ver onde vc alterou, oq?
