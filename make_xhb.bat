9059ee793514d24b5a802317795d195003d98ae8
server\leonardo <server\leonardo@cbffd26a-6331-11de-882d-8b46ad2f25d3>
2009-04-23 05:06:47 +0000

    Atualização do script de compilação com xHarbour comercial. Foi adicionado a opção de gerar a LIB da DLL(LIBPQ.DLL) usando o xLib.exe

--- make_xhb.bat
+++ make_xhb.bat
@@ -1,2 +1,30 @@
-set XHB_PATH=c:\xhb
-xbuild sqllib.lib.xbp
+@echo off
+
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
+
+if not exist libpq.dll goto ERRO_1
+
+:BUILD_OK
+   set XHB_PATH=c:\xhb\bin
+   xbuild sqllib.lib.xbp
+   xlib libpq.dll /out:lib\libpq.lib
+   goto EXIT
+
+:ERRO_1
+   @echo.
+   @echo.
+   @echo Atencao, copie a seguinte DLL para dentro dessa pasta: LIBPQ.DLL
+   @echo Essa DLL pode ser encontrada dentro da pasta de instalação do POSTGRESQL
+   @echo EX: C:\Arquivos de Programas\PostgreSql\LIB
+   @echo.
+   Pause
+   goto EXIT
+
+:CLEAN
+   del /s *.bak
+   del /s *.tds
+   del /s *.map
+   del /s *.log
+
+:EXIT
