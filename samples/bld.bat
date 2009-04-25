8526ee53327a9de4de3a241270569b1d928e0a4d
server\leonardo <server\leonardo@cbffd26a-6331-11de-882d-8b46ad2f25d3>
2009-04-25 20:04:57 +0000

    adicionado novo arquivo BLD.BAT para auxiliar a compilação de exemplos. Removido o MAKE.BAT Atualizado os caminho das LIB em SQLLIB.HBP

---
+++ samples/bld.bat
@@ -0,0 +1,22 @@
+@echo off
+if "%1" == "" goto ERRO_1
+if exist %1.EXE del %1.EXE
+
+hbmk2 -trace %1.prg -DHB_TR_LEVEL=HB_TR_ALWAYS > bld.log
+if errorlevel 1 goto BUILD_ERR
+goto EXIT
+
+:BUILD_ERR
+   notepad bld.log
+   goto EXIT
+
+:ERRO_1
+   CLS
+   @echo.
+   @echo Favor Informar o nome do arquivo EX:
+   @echo BLD TESTE1
+   @echo.
+   PAUSE
+   goto EXIT
+
+:EXIT
